a523c7013450e1ef6c674554ec78832d1b7f23d3
knorwood0 <56272255+knorwood0@users.noreply.github.com>
2021-04-21 21:18:47 +0200

    added genome reference index opt

--- RNAprocess.sh
+++ RNAprocess.sh
@@ -39,9 +39,9 @@ else
 			;;
 		-c|--c)
 			confFile=$2
-			. /$confFile
+			. $confFile
 			shift
-			;; 
+			;;
 		-o|--o)
                 	if [ -d "$2" ]; then
 				inoutdir="$2"; echo "Input Directory : $inoutdir"
@@ -66,6 +66,11 @@ else
 			refGenomeIndex=$2
 			shift
 			;;
+
+		-i|--i)
+			refIndex=$2
+			shift
+			;;
 		*)
 			echo; echo "$1 $2 is not an appropriate argument, please see usage instructions : "; usage; exit
 	esac
@@ -76,22 +81,23 @@ fi
 #Log File:
 echo >> stdout.txt
 echo $(date) >> stdout.txt
-echo >> std.out
+echo >> stdout.txt
 
 #--------------------
 # Downloading Fastq Files:
 #--------------------
 
+# TO DO: Test this section once again, made updates (20.04.2021)
 echo
 echo " 1 - Data Acquisition "
 
-if [ $download==true ]; then
+if $download ; then
 	echo "Fastq-Dump "$(date) >> stdout.txt
 	if [ ! -f $accFileName ]; then
 	        echo ; echo "Please provide the name of  a valid SRR file in .csv format"; echo; exit
 	fi
 
-	if [ $pairedEnd==true ]; then
+	if $pairedEnd ; then
 		for i in $(cut -d "," -f1 $accFileName); do
 			fast1-dump -I --split-files -O $inoutdir "$i" >> stdout.txt
 		done
@@ -117,18 +123,56 @@ echo " 2 - Data PreProcessing: Initial Quality Metrics "
 echo
 echo " 3 - Alignment "
 
-if [ $refGenomeIndex==true ]; then # TO DO: Make it so that the reference gene index does not just get saved in the same file as everything else
-	echo "Generating Reference Genome Index "$(date) >> stdout.txt
-	# Downloading Reference Genome
-	wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz -P $inoutdir; gzip -d $inoutdir/hg38.fa.gz
-	# Downloading Annotation File
-	wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/genes/hg38.ensGene.gtf.gz -P $inoutdir; gzip -d $inoutdir/hg38.ensGene.gtf.gz
-	# Creating reference index - note that the number of threads can be adjusted accordingly here
-	STAR --runMode genomeGenerate --runThreadN 1 --genomeDir $inoutdir --readFilesCommand gunzip -c --genomeFastaFiles $inoutdir/hg38.fa.gz --sjdbGTFfile $inoutdir/hg38.ensGene.gtf.gz
+# TO DO: Remove gzip from .fa/.gtf file downloads, reference index wont work otherwise even with --readFilesCommand gunzip -c
 
-	echo "Genome Index Created "$(date) >> stdout.txt
+# Generating a Genome Reference Index:
+if $refGenomeIndex ; then
+	if [ ! -d $inoutdir/hg38 ]; then
+		mkdir $inoutdir/hg38
+                echo "Generated Directory "$inoutdir"/hg38"
+	else
+		echo $inoutdir"/hg38 exists, continuing."
+	fi
+
+	echo ; echo "Generating Genome Index"; echo "Generating Reference Genome Index "$(date) >> stdout.txt
+	# Downloading Reference Genome
+	if [ ! -f $inoutdir/hg38/hg38.fa.gz ]; then
+		wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz -P $inoutdir/hg38/ >> stdout.txt
+	fi
+	# Downloading Annotation File
+	if [ ! -f $inoutdir/hg38/hg38.ensGene.gtf.gz ]; then
+		wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/genes/hg38.ensGene.gtf.gz -P $inoutdir/hg38/ >> stdout.txt
+	fi
+	# Creating reference index - note that the number of threads can be adjusted accordingly here
+	if [ -f $inoutdir/hg38/hg38.fa ] && [ -f $inoutdir/hg38/hg38.ensGene.gtf ]; then
+		STAR --runMode genomeGenerate --runThreadN 4 --genomeDir $inoutdir/hg38/ --genomeFastaFiles $inoutdir/hg38/hg38.fa --sjdbGTFfile $inoutdir/hg38/hg38.ensGene.gtf >> stdout.txt # 2>> stderr.txt
+	fi
+
+	refIndex=$inoutdir/hg38
+
+	echo ; echo "Complete"; echo "Genome Index Created "$(date) >> stdout.txt
+
 fi
 
 # Alignment with STAR:
-#for [ i".fa" in $inoutdir ] # or something like this to only take fast files, may be easier to make a new fast file directory earlier
+echo "Generating list of files for mapping of multiple in one run"$(date) >> stdout.txt
 
+files1=$inoutdir/*1.fastq; set -- $files1
+files2=$inoutdir/*2.fastq; set -- $files2
+starFiles1=""
+starFiles2=""
+for i in $files1; do starFiles1="${starFiles1},$i"; done
+for i in $files2; do starFiles2="${starFiles2},$i"; done
+starFiles1="${starFiles1:1}"; starFiles2="${starFiles2:1}"
+
+echo "Read 1 list: "$starFiles1"\nRead 2 list: "$starFiles2 >> stdout.txt
+
+if [ ! -d $inoutdir/alignments }; then mkdir $inoutdir/alignments; fi
+
+STAR --runThreadN 4 --readFilesCommand gunzip -c --genomeDir $refIndex --readFilesIn $starFiles1 $starFiles2 --outFilterMultimapNmax 1 \
+	--outSAMtype BAM SortedByCoordinate --quantMode GeneCounts --outFileNamePrefix $inoutdir/alignments 2>> stderr.txt
+
+# Note the fastq files are not in gzip format post accession step - need to zip them prior to this alignment step
+# Not finding the enome file /home/knorwood/data/GSE157103/GSE157103/hg38//genomeParameters.txt
+# TO DO: Need a --outSAMattrRGline for corresponding read groups? (in the above STAR alignment command)
+# TO DO: Dont think its necessary to add --clip5pNbases but need to double check, will need to make this so that it could be user input?
